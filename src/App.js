bb54f2f9cc71c0e5f24201e8e003901ed431cec8
gamadv <lealdev.py@gmail.com>
2019-07-25 22:01:30 -0300

    Componentização: Criação do Header, estilos CSS e alterações

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import './style.css';
 
-
-function App() {
-  return (
+const App = () => (
+  
     <div className="App">
-      <h1> Iaeew seus Pull Towww</h1>
+      <Header/>
     </div>
-  );
-}
+  
+);
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+//Imports
+import React from 'react';
+import './styles.css';
+
+/* Stateless component, criando component com apenas funções
+*/
+const Header = () => 
+<header id="main-header">JSHunt</header>
+
+
+
+export default Header;
