59b6ef6fba69b6a2531a30674e469b9edf4d48f9
Moacir Junior/LGESP RBrisatc601.juniorD MC PVG(brisatc601.junior@lgepartner.com) <brisatc601.junior@lge.com>
2019-08-13 16:55:37 -0300

    Começando rotas de páginas

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Main from './Pages/main';
+
+const Routes = () =>(
+    <BrowserRouter>
+        <Switch>
+
+
+        </Switch>
+    </BrowserRouter>
+)
+
+
+
+/*
+Switch: Serve para definir que a página
+retorne 1 link por vez.
+ */
